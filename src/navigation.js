c8a60b9ad3522c6e8d4bdc20690ded6ba9935e9e
shwilks <sw463@cam.ac.uk>
2025-01-18 00:08:11 +1100

    Update navigation

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,6 @@
 
 import * as THREE from 'three';
+import { ArcballControls } from 'three/addons/controls/ArcballControls.js';
 
 export const NavMethods = {
 
@@ -9,142 +10,104 @@ export const NavMethods = {
     },
 
     bindNavigation(){
+        
+        var addCameraControls = (camera) => {
+
+            const controls = new ArcballControls( 
+                camera.get3JSCamera(),
+                this.viewport.canvas,
+                this.scene.get3JSScene()
+            );
+    
+            controls.setGizmosVisible(false);
+            controls.unsetMouseAction('WHEEL', 'SHIFT');
+            controls.unsetMouseAction(1, 'SHIFT');
+    
+            window.addEventListener('keydown', (event) => {
+                if (event.key === 'Shift') {
+                    console.log("UNSET");
+                    controls.unsetMouseAction('WHEEL', null);
+                }
+            });
+    
+            window.addEventListener('keyup', (event) => {
+                if (event.key === 'Shift') {
+                    console.log("SET");
+                    controls.setMouseAction('ZOOM', 'WHEEL', null);
+                }
+            });
+
+            controls.addEventListener( 'change', () => {
+
+                this.scene.showhideDynamics(this.camera);
+                this.render();
+    
+            } );
+
+            return controls;
+
+        }
+
+        this.perspcontrols = addCameraControls(this.camera.perspcamera);
+        this.orthocontrols = addCameraControls(this.camera.orthocamera);
+
+        // controls.setMouseAction('ROTATE', 1, 'SHIFT');
+
+        // // Create a DirectionalLight
+        // const light = new THREE.DirectionalLight(0xffffff, 1);
+        // light.position.set(0, 1, -1); // Set the light's relative position
+        // this.camera.get3JSCamera().add(light); // Add the light to the camera
+
+        // // Add the camera (with the light) to the scene
+        // this.scene.get3JSScene().add(this.camera.get3JSCamera());
+
+        
+
+        // this.controls = controls;
 
         // Add viewport variables
         this.navigable = true;
-        this.damper    = {};
+        // this.damper    = {};
     
-        // Set bindings
-        this.mouseMove        = this.rotateSceneWithInertia;
-        this.mouseMoveMeta    = this.panScene;
-        this.mouseScroll      = this.zoomScene;
-        this.mouseScrollShift = this.rockScene;
+        // // Set bindings
+        // this.mouseMove        = this.rotateSceneWithInertia;
+        // this.mouseMoveMeta    = this.panScene;
+        // this.mouseScroll      = this.zoomScene;
+        // this.mouseScrollShift = this.rockScene;
     
-        // Bind mouse events
-        this.viewport.canvas.addEventListener("mousemove", function(event){ 
+        // // Bind mouse events
+        // this.viewport.canvas.addEventListener("mousemove", function(event){ 
     
-            var viewer   = this.viewport.viewer;
-            var viewport = this.viewport;
+        //     var viewer   = this.viewport.viewer;
+        //     var viewport = this.viewport;
     
-            if(viewer.navigable){
-                if(viewport.mouse.down && !viewport.dragObject && viewer.scrollFocus()){
-                    if(!viewport.mouse.event.metaKey && !viewport.mouse.event.shiftKey && viewport.touch.num <= 1){
-                        viewer.mouseMove();
-                    } else if(viewport.mouse.event.metaKey){
-                        viewer.mouseMoveMeta(this.viewport.mouse.deltaX, this.viewport.mouse.deltaY);
-                    }
-                }
-            }
-            // if(this.viewport.mouse.down){
-            //     this.viewport.viewer.mouseMove();
-            // }
+        //     if(viewer.navigable){
+        //         if(viewport.mouse.down && !viewport.dragObject && viewer.scrollFocus()){
+        //             if(!viewport.mouse.event.metaKey && !viewport.mouse.event.shiftKey && viewport.touch.num <= 1){
+        //                 viewer.mouseMove();
+        //             } else if(viewport.mouse.event.metaKey){
+        //                 viewer.mouseMoveMeta(this.viewport.mouse.deltaX, this.viewport.mouse.deltaY);
+        //             }
+        //         }
+        //     }
+        //     // if(this.viewport.mouse.down){
+        //     //     this.viewport.viewer.mouseMove();
+        //     // }
     
-        });
+        // });
     
         this.viewport.canvas.addEventListener("wheel", function(event){
-    
+            
             var viewer   = this.viewport.viewer;
             var viewport = this.viewport;
-    
+            
             if(viewer.navigable && viewer.scrollFocus()){
                 if(viewport.mouse.scrollShift){
                     viewer.mouseScrollShift();
-                } else {
-                    viewer.mouseScroll(viewport.mouse.scrollY);
                 }
             }
     
         });
-        // this.div.addEventListener("touchmove", function(){
-        //     if(this.touch.num == 1){
-        //         navMouseMove(this); 
-        //     } else {
-        //         navScroll(this);
-        //     }
-        // });
-    
-    },
-
-    rotateSceneWithInertia(){
-
-        var deltaX = this.viewport.mouse.deltaX;
-        var deltaY = -this.viewport.mouse.deltaY;
-    
-        this.rotateSceneXY(deltaX, deltaY);
-    
-        if(this.rotationDamperTimeout){ 
-            window.clearTimeout( this.rotationDamperTimeout );
-        }
-        this.rotationDamper( deltaX, deltaY );
-    
-    },
-
-    rotateSceneXY( deltaX, deltaY, linked ){
-
-        // Rotate linked plots
-        if (linked === undefined) {
-            this.linkedviewers.map(viewer => viewer.rotateSceneXY(deltaX, deltaY, true));
-        }
-    
-        this.sceneChange = true;
-        this.scene.rotateOnAxis(new THREE.Vector3(0,1,0), deltaX );
-        this.scene.rotateOnAxis(new THREE.Vector3(1,0,0), deltaY );
-        if(this.scene.dynamic) this.scene.showhideDynamics(this.camera);
-    
-    },
-    
-    rotationDamper(deltaX, deltaY){
-    
-        if(Math.abs(deltaX) > 0.001 || Math.abs(deltaY) > 0.001){
-    
-            deltaX = deltaX*0.85;
-            deltaY = deltaY*0.85;
-            this.rotateSceneXY(deltaX, deltaY);
-            var viewer = this;
-            this.rotationDamperTimeout = window.setTimeout(function(){
-                if(!viewer.viewport.mouse.down){
-                    viewer.rotationDamper( deltaX, deltaY )
-                }
-            }, 10);
-    
-        }
-    
-    },
-    
-    zoomScene(zoom, linked){
-    
-        // Zoom linked plots
-        if (linked === undefined) {
-            this.linkedviewers.map(viewer => viewer.zoomScene(zoom, true));
-        }
-    
-        this.sceneChange = true;
-        this.camera.zoom(Math.pow(1.01, zoom));
-    
-    },
-    
-    panScene(panX, panY, linked){
-    
-        // Pan linked plots
-        if (linked === undefined) {
-            this.linkedviewers.map(viewer => viewer.panScene(panX, panY, true));
-        }
-    
-        this.sceneChange = true;
-    
-        var plotHolder = this.scene.plotHolder;
-        var plotPoints = this.plotPoints;
-    
-        var position = new THREE.Vector3(0, 0, 0);
-        position.applyMatrix4(plotHolder.matrixWorld).project(this.camera.get3JSCamera());
-        position.x += panX;
-        position.y += panY;
-        var inverse_mat = new THREE.Matrix4();
-        inverse_mat.copy(plotHolder.matrixWorld).invert();
-        position.unproject(this.camera.get3JSCamera()).applyMatrix4(inverse_mat);
-    
-        this.scene.panScene(position.toArray());
-        this.scene.showhideDynamics(this.camera);
     
     },
     
